8b0ab9c195e2f11fcb15c598adf6074a9440731d
Bikal <bikaladhikari5@gmail.com>
2024-03-30 09:46:45 +1100

    form onsubmit and prevent default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const [headingText, setHeadingText] = useState("");
+  const handlechange = (event) => {
+    setName(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    setHeadingText(name);
+    event.preventDefault();
+  };
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handlechange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
